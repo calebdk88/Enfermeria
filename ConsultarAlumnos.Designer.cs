b1942f07aec22e095f36a20c400dcaa010ea818f
JuanDavidEB <jdavideb@hotmail.com>
2014-12-05 11:57:48 -0600

    Consultas Autocompletado / Autofiltro
    ps aaaa

--- ConsultarAlumnos.Designer.cs
+++ ConsultarAlumnos.Designer.cs
@@ -28,58 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtConsultas = new System.Windows.Forms.TextBox();
+            this.dgvConsultas = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultas)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtConsultas
             // 
-            this.textBox1.Location = new System.Drawing.Point(139, 110);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtConsultas.Location = new System.Drawing.Point(271, 142);
+            this.txtConsultas.Name = "txtConsultas";
+            this.txtConsultas.Size = new System.Drawing.Size(110, 20);
+            this.txtConsultas.TabIndex = 0;
+            this.txtConsultas.TextChanged += new System.EventHandler(this.txtConsultas_TextChanged);
             // 
-            // dataGridView1
+            // dgvConsultas
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 177);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(377, 150);
-            this.dataGridView1.TabIndex = 1;
+            this.dgvConsultas.AllowUserToAddRows = false;
+            this.dgvConsultas.AllowUserToDeleteRows = false;
+            this.dgvConsultas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultas.Location = new System.Drawing.Point(12, 177);
+            this.dgvConsultas.Name = "dgvConsultas";
+            this.dgvConsultas.ReadOnly = true;
+            this.dgvConsultas.Size = new System.Drawing.Size(668, 197);
+            this.dgvConsultas.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(159, 137);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(268, 112);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.Size = new System.Drawing.Size(113, 18);
             this.label1.TabIndex = 2;
-            this.label1.Text = "ps aaaa";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(150, 81);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.Text = "Filtrar Nombre :";
             // 
             // ConsultarAlumnos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(401, 339);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(692, 386);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgvConsultas);
+            this.Controls.Add(this.txtConsultas);
             this.Name = "ConsultarAlumnos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ConsultarAlumnos";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,9 +82,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtConsultas;
+        private System.Windows.Forms.DataGridView dgvConsultas;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
     }
 }
--- ConsultarAlumnos.cs
+++ ConsultarAlumnos.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Usando la referencia para conectarnos a MySQL
+
+using MySql.Data.MySqlClient;
+
 namespace Enfermeria
 {
     public partial class ConsultarAlumnos : Form
@@ -15,6 +19,119 @@ namespace Enfermeria
         public ConsultarAlumnos()
         {
             InitializeComponent();
+            
+            // Metodo para autocompletado iniciado al cargar la forma
+
+            AutoCompletadoTextBox();
+
+            // Metodo autofiltrado iniciado al cargar la forma
+            AutoFiltradoDGV();
         }
-    }
+
+        // Declare esta variable hasta arriba debido a que la utilizo 2 veces
+        // para el autofiltrado en el dgv y en la filtracion con el textbox.
+
+        DataTable Tabla;
+
+        void AutoCompletadoTextBox()
+        {
+            /* David
+             * 
+             * Codigo para generar el autocompletado */
+
+            // llamamos a la clase conexion y su metodo conectar para abrir la conexion
+                conexion.conectarme();
+
+            // Lo siguiente son propiedades de un textbox para el autocompletado
+
+                txtConsultas.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                txtConsultas.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                AutoCompleteStringCollection coll = new AutoCompleteStringCollection();
+
+            /* el comando se utiliara para mostrar los nombres en la lista de sugerencias de 
+             * autocompletado cuando se teclee algo */
+
+                string consulta = "SELECT nombre FROM alumnos;";
+
+            /* Establecemos la variable que utilizaremos para efectuar el comando,
+             * y a su vez, se le dice hacia donde mandamos la orden (en la base de datos) */
+                MySqlCommand comandoconsulta = new MySqlCommand(consulta, conexion.conectarme());
+
+            /*el lector se utiliza para efectuar una lectura de datos*/
+                MySqlDataReader lector;
+
+                try 
+                {
+                    /*el lector tomara el comando SQL*/
+                    lector = comandoconsulta.ExecuteReader();
+
+                    while (lector.Read())
+                    {
+                        /*Mientras la lectura ocurra...que se obtiene el valor
+                         * de la tabla del campo nombre*/
+                        string Snombre = lector.GetString("nombre");                 
+                        coll.Add(Snombre);
+                    }
+
+                }
+                
+                catch(Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+
+                }
+                
+                // Al final, se realiza el autocompletado de cadenas de la fuente que seleccionamos
+                txtConsultas.AutoCompleteCustomSource = coll;
+        }
+
+        void AutoFiltradoDGV()
+        {
+        
+        // Codigo para filtrado de informacion en el DGV con el textbox
+            conexion.conectarme();
+
+        // Le decimos el comando que queremos ejecutar
+
+            MySqlCommand comandoDB = new MySqlCommand("SELECT matricula, nombre, apellido, idgrado, idgrupo, correo FROM alumnos ORDER BY nombre;", conexion.conectarme());
+
+            try
+            {
+
+                MySqlDataAdapter lector = new MySqlDataAdapter();
+                lector.SelectCommand = comandoDB;
+                Tabla = new DataTable();
+                lector.Fill(Tabla);
+                BindingSource bFuente = new BindingSource();
+
+                bFuente.DataSource = Tabla;
+                dgvConsultas.DataSource = bFuente;
+                lector.Update(Tabla);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void txtConsultas_TextChanged(object sender, EventArgs e)
+        {
+
+            // Establezco la variable DV utilizado la varible global Tabla
+
+            DataView DV = new DataView(Tabla);
+
+            // Aqui filtro mediante el texbox, lo que sea que escriba me lo muestra
+            DV.RowFilter = string.Format("nombre LIKE '%{0}%'", txtConsultas.Text);
+            // le pongo de donde obtendra la fuente de filtrado el dgv
+            dgvConsultas.DataSource = DV;
+
+        }
+        
+        
+        }
 }
